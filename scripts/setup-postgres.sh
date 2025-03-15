9a3c65a5abdb92873f616aa6bf60170dd87b92dc
pierre aubert <pierre@spinorama.org>
2025-03-15 11:00:28 +0100

    fix: pg startup file

--- scripts/setup-postgres.sh
+++ scripts/setup-postgres.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-PG=/data/dot-node/pg
-mkdir -p ${PG}/log ${PG}/run
+PG=/polkadot/postgres_data
+mkdir -p ${PG}/logs ${PG}/run
 
 # create a PG for dotlake
-pg_createcluster -u pierre -g pierre -d ${PG}/16/dotlake -l $PG/log -p 5434 16 dotlake
+pg_createcluster -u pierre -g pierre -d ${PG}/dotlake -l ${PG}/logs/dotlake.log -s ${PG}/run -p 5434 16 dotlake
 sudo systemctl restart postgresql@16-dotlake.service
 sudo systemctl status postgresql@16-dotlake.service
 
 # create table
-createdb -h /tmp -p 5434 dotlake
+createdb -h ${PG}/run -p 5434 dotlake
 
 # create users
-createuse -h /tmp -p 5434 --createdb --createdb --pwprompt dotlake
+createuse -h ${PG}/run -p 5434 --createdb --createdb --pwprompt dotlake
 
-psql -h /tmp -p 5434 -f etc/postgresql/pg.sql
+psql -h ${PG}/run -p 5434 -d dotlake -f etc/postgresql/pg.sql
