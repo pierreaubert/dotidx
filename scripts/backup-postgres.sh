3c48d5bb3459b763b36e881695953e17c4ba214f
Pierre F. Aubert <1709343+pierreaubert@users.noreply.github.com>
2025-03-21 09:49:38 +0100

    added a first batch of files for systemd

--- scripts/backup-postgres.sh
+++ scripts/backup-postgres.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
-
-PG=/polkadot/postgres_data
+export PG=/polkadot/postgres_data
 for year in 2020 2021 2022 2023 2024 2025; do
     for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
-        echo "Dumping ${year}_${month}"
-        table="chain.blocks_polkadot_polkadot_${year}_${month}"
-        pg_dump -h ${PG}/run -p 5434 -n chain -Z -t "${table}" dotidx > "${table}.dump.gz"
+	for chain in polkadot assethub; do
+	    for table in blocks address2blocks; do
+		tablename="chain.blocks_polkadot_${chain}_${year}_${month}"
+		dumpfile=${tablename}.dump
+		if test ! -f "${dumpfile}"; then
+		    echo "Dumping to $dumpfile"
+		    pg_dump -h ${PG}/run -p 5434 -n chain -t "${tablename}" dotidx > "${dumpfile}"
+      	        fi
+	    done
+	done
     done
 done
