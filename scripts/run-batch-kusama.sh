2f73a209048cbb2e6770ac18e6eebf550bd5f1aa
Pierre F. Aubert <1709343+pierreaubert@users.noreply.github.com>
2025-05-02 17:28:39 +0200

    debug access from prometheus

--- scripts/run-batch-kusama.sh
+++ scripts/run-batch-kusama.sh
@@ -3,5 +3,5 @@
 DIX=~/src/dotidx
 
 for chain in kusama assethub people collectives acurast; do
-    $DIX/dixbatch -conf $DIX/conf/conf-horn.toml -chain $chain -relayChain kusama
+    $DIX/bin/dixbatch -conf $DIX/conf/conf-horn.toml -chain $chain -relayChain kusama
 done
--- scripts/run-batch-polkadot.sh
+++ scripts/run-batch-polkadot.sh
@@ -3,5 +3,5 @@
 DIX=~/src/dotidx
 
 for chain in polkadot assethub people collectives frequency mythos; do
-    $DIX/dixbatch -conf $DIX/conf/conf-horn.toml -chain $chain -relayChain polkadot
+    $DIX/bin/dixbatch -conf $DIX/conf/conf-horn.toml -chain $chain -relayChain polkadot
 done
