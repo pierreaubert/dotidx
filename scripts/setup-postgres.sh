94732362162612b1fa9a38306a0f1f77668f91b9
Pierre F. Aubert <1709343+pierreaubert@users.noreply.github.com>
2025-10-29 06:48:52 +0000

    fix partially postgres setup

--- scripts/setup-postgres.sh
+++ scripts/setup-postgres.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-PG=/polkadot/postgres_data
-mkdir -p ${PG}/logs ${PG}/run
-chown -R pierre:pierre ${PG}
+PG=/dotidx/db
+mkdir -p ${PG}
+TS=/dotidx/ts
+mkdir -p ${PG}/slow0 ${PG}/slow1 ${PG}/slow2 ${PG}/slow3 ${PG}/slow4 ${PG}/slow5
+mkdir -p ${PG}/fast0 ${PG}/fast1 ${PG}/fast2 ${PG}/fast3
+sudo chown postgres:postgre ${PG} ${TS}
 
 # create a PG for dotidx
-pg_createcluster -u pierre -g pierre -d ${PG}/dotidx -l ${PG}/logs/dotidx.log -s ${PG}/run -p 5434 16 dotidx
+sudo pg_createcluster -d ${PG}/dotidx -p 5434 16 dotidx
 sudo systemctl start postgresql@16-dotidx.service
 sudo systemctl status postgresql@16-dotidx.service
 sudo systemctl enable postgresql@16-dotidx.service
 sudo systemctl status postgresql@16-dotidx.service
 
 # create table
-createdb -h ${PG}/run -p 5434 dotidx
+sudo su postgres createdb -h ${PG}/run -p 5434 dotidx
 
 # create users
-createuser -h ${PG}/run -p 5434 --createdb --createdb --pwprompt dotidx
+sudo su postgres createuser -h ${PG}/run -p 5434 --createdb --createdb --pwprompt dotidx
 
-psql -h ${PG}/run -p 5434 -d dotidx -f etc/postgresql/pg.sql
+sudo su postgres psql -h ${PG}/run -p 5434 -d dotidx -f etc/postgresql/pg.sql
